4368eec009300557e1bde1c5576eabdcb2e2b3c1
Novy <novyvictor@gmail.com>
2017-05-26 23:49:47 -0300

    Add youtube key and create the search_bar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyChlDN2uJApNbwFMMeL1AxW86kLfCwyKB8';
+
 const App = () => {
-    return <div>Hello!!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
